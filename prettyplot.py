14309c39dde37c7dac9aa404e953087e88b1b2eb
mimocha <l.chawit@outlook.com>
2019-12-22 01:44:52 +0000

    Added Kwargs normalizing function as helper function. Reformatted ScatterLinear and ScatterLog.

--- prettyplot.py
+++ prettyplot.py
@@ -1,102 +1,182 @@
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 import numpy as np
+
+"""
+Helper function to normalize kwargs input
+"""
+def NormKwarg(kwargs):
+	return {k.lower():v for k,v in kwargs.items()}
+
 
 """
 Collection of pretty plot functions.
 """
 class PrettyPlot(object):
-	"""docstring for ."""
 
-	def __init__(self, arg):
-		super(, self).__init__()
-		self.arg = arg
+	"""
+	docstring for .
+	"""
+	def __init__(self):
+		# super(, self).__init__()
 		return
 
 	"""
-	Temporary
+	Plotting Error Margin
 	"""
-	def fillbetween (self, **kwargs):
-		# Error margin as area
-		if kwargs.get('eflag', True):
-			upper_error = y[:,0] + y[:,2]
-			lower_error = y[:,0] - y[:,2]
-			lim_idx = (lower_error <= 0)
-			lower_error[lim_idx] = 0
+	def fillbetween (self, x, yMean, yStd, **kwargs):
+		upper_error = yMean + yStd
+		lower_error = yMean - yStd
+		lim_idx = (lower_error <= 0)
+		lower_error[lim_idx] = 0
 
-			self.plt.plot (x, upper_error, color='blue', dashes=[8,4], alpha=0.25, label='upper error margin')
-			self.plt.plot (x, lower_error, color='blue', dashes=[8,4], alpha=0.25, label='lower error margin')
-			self.plt.fill_between (x, upper_error, lower_error, facecolor='blue', alpha=0.15, label='error margin')
+		plt.plot(x, upper_error,
+				color=kwargs.get('edgecolor', 'blue'),
+				dashes=kwargs.get('dashes', [8,4]),
+				alpha=kwargs.get('edgealpha', 0.25),
+				label=kwargs.get('labeledgeupper', 'upper error margin'))
+		plt.plot(x, lower_error,
+				color=kwargs.get('edgecolor', 'blue'),
+				dashes=kwargs.get('dashes', [8, 4]),
+				alpha=kwargs.get('edgealpha', 0.25),
+				label=kwargs.get('labeledgelower', 'lower error margin'))
+		plt.fill_between(x, upper_error, lower_error,
+				facecolor=kwargs.get('facecolor', 'blue'),
+				alpha=kwargs.get('facealpha', 0.15),
+				label=kwargs.get('labelface', 'error margin'))
+
 		return
 
 	"""
 	Scatter Plot with Polynomial Trendline
+	
+	kwargs list (case insensitive):
+		DataColor = 'color'
+		DataLabel = 'label'
+		Figax = (fig, ax)
+		FigSize = (int width, int height)
+		LegendLocation = 'location'
+		Title = 'title'
+		Trendline = bool True/False
+		TrendlineAlpha = float alpha
+		TrendlineColor = 'color'
+		TrendlineDegree = int
+		TrendlineLabel = 'label'
+		TrendlineStyle = 'style'
+		xlabel = 'xlabel'
+		ylabel = 'ylabel'
 	"""
 	def ScatterLinear (self, x, y, **kwargs):
-		self.fig, self.ax = self.plt.subplots(figsize=kwargs.get('figsize',(16,8)))
+		kwargs = NormKwarg(kwargs)
+		(fig, ax) = kwargs.get('figax', plt.subplots(figsize=kwargs.get('figsize',(16,8))))
+		plt.sca(ax)
 
-		self.plt.scatter (x, y, color='orange', label=kwargs.get('dataname', 'Data'))
+		plt.scatter (x, y,
+					 color=kwargs.get('datacolor','Orange'),
+					 label=kwargs.get('datalabel','Data'))
 
-		self.ax.set_xlabel(kwargs.get('xlabel', 'X'))
-		self.ax.set_ylabel(kwargs.get('ylabel', 'Y'))
-		self.ax.set_title(kwargs.get('title', 'Scatter Plot'))
+		ax.set_xlabel(kwargs.get('xlabel', 'X'))
+		ax.set_ylabel(kwargs.get('ylabel', 'Y'))
+		ax.set_title(kwargs.get('title', 'Scatter Plot'))
 
-		# Best Fit Curve
-		c = np.polyfit(x, y[:], 3)
-		p = np.poly1d (c)
-		l = np.linspace (x[0], x[-1], num=100)
-		self.plt.plot (l, p(l), '--', color='black', alpha=0.75, label='Best Fit Curve')
+		# Trendline
+		if kwargs.get('trendline',False) == True:
+			deg = kwargs.get('trendlinedegree', 1)
+			C = np.polyfit(x, y, deg)
+			P = np.poly1d (C)
+			L = np.linspace (x[0], x[-1], num=100)
+			plt.plot (L, P(L),
+					  kwargs.get('trendlinestyle','--'),
+					  alpha=kwargs.get('trendlinealpha', 0.75),
+					  color=kwargs.get('trendlinecolor', 'black'),
+					  label=kwargs.get('trendlinelabel', 'Trendline'))
+			equation = ''
+			print ('Precise coefficient values:',end='')
+			for i,c in enumerate(C):
+				if abs(c) >= 1e-6:
+					equation += f' {c:+.6f}*x^{deg - i}'
+				print (f' {c} |',end='')
+			print (f'\nEquation for trendline is: {equation}')
 
-		self.ax.legend(loc='upper left')
-		self.plt.grid()
-		self.plt.show()
+		if kwargs.get('legend',False) == True:
+			ax.legend(loc=kwargs.get('legendlocation','upper left'))
+		if kwargs.get('grid',False) == True:
+			plt.grid()
 
-		equation = f'{c[0]:.2e}*x^3 {c[1]:+.2e}*x^2 {c[2]:+.2e}*x {c[3]:+.2e}'
-		print (f'Approximate Equation for line of best fit is: {equation}')
-		print (f'Precise coefficient values: {c[0]} | {c[1]} | {c[2]} | {c[3]}')
-
-		return (self, c)
+		return (fig, ax)
 
 	"""
 	Logarithmic Scatter Plot with Trendline
+	
+	kwargs list (case insensitive):
+		DataColor = 'color'
+		DataLabel = 'label'
+		Figax = (fig, ax)
+		FigSize = (int width, int height)
+		LegendLocation = 'location'
+		Title = 'title'
+		Trendline = bool True/False
+		TrendlineAlpha = float alpha
+		TrendlineColor = 'color'
+		TrendlineDegree = int
+		TrendlineLabel = 'label'
+		TrendlineStyle = 'style'
+		xlabel = 'xlabel'
+		ylabel = 'ylabel'
 	"""
 	def ScatterLog (self, x, y, **kwargs):
-		self.fig, self.ax = self.plt.subplots(figsize=kwargs.get('figsize',(16,8)))
+		kwargs = NormKwarg(kwargs)
+		(fig, ax) = kwargs.get('figax', plt.subplots(figsize=kwargs.get('figsize',(16,8))))
+		plt.sca(ax)
 
 		# Zero checks & converts to log
 		# Removes any data point where y[mean] is zero as it breaks this function
-		if (np.min(y[:]) == 0):
+		if (np.min(y) == 0):
 			zflag = True
-			zidx = (y[:] != 0)
+			zidx = (y != 0)
 			xlog = np.log10(x[zidx])
-			ylog = np.log10(y[zidx,0])
+			ylog = np.log10(y[zidx])
 		else:
 			zflag = False
 			xlog = np.log10(x)
-			ylog = np.log10(y[:,0])
+			ylog = np.log10(y)
 
-		self.plt.scatter (xlog, ylog, color='orange', label=kwargs.get('dataname', 'Data'))
+		plt.scatter (xlog, ylog,
+					 color=kwargs.get('datacolor', 'orange'),
+					 label=kwargs.get('datalabel', 'Data'))
 
-		self.ax.set_xlabel(kwargs.get('xlabel', 'X'))
-		self.ax.set_ylabel(kwargs.get('ylabel', 'Y'))
-		self.ax.set_title(kwargs.get('title', 'Logarithmic Scatter Plot'))
+		ax.set_xlabel(kwargs.get('xlabel', 'X'))
+		ax.set_ylabel(kwargs.get('ylabel', 'Y'))
+		ax.set_title(kwargs.get('title', 'Logarithmic Scatter Plot'))
 
-		# Best Fit Curve
-		c = np.polyfit(xlog, ylog, 1)
-		p = np.poly1d (c)
-		l = np.linspace (xlog[0], xlog[-1], num=100)
-		self.plt.plot (l, p(l), '--', color='black', alpha=0.75, label='Best Fit Line')
+		# Trendline
+		if kwargs.get('trendline',False) == True:
+			deg = kwargs.get('trendlinedegree', 1)
+			C = np.polyfit(xlog, ylog, deg)
+			P = np.poly1d (C)
+			L = np.linspace (xlog[0], xlog[-1], num=100)
+			plt.plot (L, P(L),
+					  kwargs.get('trendlinestyle','--'),
+					  alpha=kwargs.get('trendlinealpha', 0.75),
+					  color=kwargs.get('trendlinecolor', 'black'),
+					  label=kwargs.get('trendlinelabel', 'Trendline'))
+			equation = ''
+			print ('Precise coefficient values:',end='')
+			for i,c in enumerate(C):
+				if abs(c) >= 1e-6:
+					equation += f' {c:+.6f}*x^{deg - i}'
+				print (f' {c} |',end='')
+			print (f'\nEquation for trendline is: {equation}')
 
-		self.ax.legend(loc='upper left')
-		self.plt.grid()
-		self.plt.show()
+		if kwargs.get('legend',False) == True:
+			ax.legend(loc=kwargs.get('legendlocation', 'upper left'))
+		if kwargs.get('grid',False) == True:
+			plt.grid()
 
-		equation = '{0[0]:.2f}*x {0[1]:+.2f}'.format(c)
-		print (f'Approximate Equation for line of best fit is: {equation}')
-		print ('Precise coefficient values: {0[0]} | {0[1]}'.format(c))
 		if zflag:
 			print ('Warning: Data contains zero-values, data partially truncated to removed zero-valued points')
 
-		return (self, c)
+		return (fig, ax)
 
 	"""
 	Linear scatter plot function for comparing multiple datasets
@@ -107,21 +187,21 @@ class PrettyPlot(object):
 	colorlist = list of strings, containing the named color for each dataset, in same order
 	"""
 	def ScatterLinearCompare (self, x, y, datalabel, colorlist, **kwargs):
-		self.fig, self.ax = self.plt.subplots(figsize=kwargs.get('figsize',(16,8)))
+		(fig, ax) = kwargs.get('figax', plt.subplots(figsize=kwargs.get('figsize',(16,8))))
 
 		# data set count
 		dsc = y.shape[1]
 
 		# y_data[i,:] = [ds1[i], ds2[i], ds3[i], ...]
 		for i in range (dsc):
-			self.plt.scatter (x, y[:,i], label=datalabel[i], alpha=0.5, color=colorlist[i])
+			plt.scatter (x, y[:,i], label=datalabel[i], alpha=0.5, color=colorlist[i])
 
 			if kwargs.get('typeflag','None') == 'linear':
 				# Best Fit Line
 				c = np.polyfit(x, y[:,i], 1)
 				p = np.poly1d (c)
 				l = np.linspace (x[0], x[-1], num=100)
-				self.plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
+				plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
 				equation = f'{c[0]:+.2e}*x {c[1]:+.2e}'
 				print (f'Approximate Equation for line of best fit is: {equation}')
 				print (f'Precise coefficient values: {c[0]} | {c[1]}')
@@ -130,20 +210,20 @@ class PrettyPlot(object):
 				c = np.polyfit(x, y[:,i], 3)
 				p = np.poly1d (c)
 				l = np.linspace (x[0], x[-1], num=100)
-				self.plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
+				plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
 				equation = f'{c[0]:+.2e}*x^3 {c[1]:+.2e}*x^2 {c[2]:+.2e}*x {c[3]:+.2e}'
 				print (f'Approximate Equation for line of best fit is: {equation}')
 				print (f'Precise coefficient values: {c[0]} | {c[1]} | {c[2]} | {c[3]}')
 
-		self.ax.set_xlabel(kwargs.get('xlabel', 'Document Size'))
-		self.ax.set_ylabel(kwargs.get('ylabel', 'Average Runtime (ms)'))
-		self.ax.set_title (kwargs.get('title', 'Performance Comparison Scatter Plot'))
+		ax.set_xlabel(kwargs.get('xlabel', 'Document Size'))
+		ax.set_ylabel(kwargs.get('ylabel', 'Average Runtime (ms)'))
+		ax.set_title (kwargs.get('title', 'Performance Comparison Scatter Plot'))
 
-		self.ax.legend(loc='upper left')
-		self.plt.grid()
-		self.plt.show()
+		ax.legend(loc='upper left')
+		plt.grid()
+		plt.show()
 
-		return (self, c)
+		return (fig, ax)
 
 	"""
 	Logarithmic scatter plot function for comparing multiple datasets
@@ -154,7 +234,7 @@ class PrettyPlot(object):
 	colorlist = list of strings, containing the named color for each dataset, in same order
 	"""
 	def ScatterLogCompare (self, x, y, datalabel, colorlist, **kwargs):
-		self.fig, self.ax = self.plt.subplots(figsize=kwargs.get('figsize',(16,8)))
+		fig, ax = plt.subplots(figsize=kwargs.get('figsize',(16,8)))
 
 		# Zero checks & converts to log
 		# Removes any data point where y[mean] is zero as it breaks this function
@@ -176,22 +256,22 @@ class PrettyPlot(object):
 
 		# y_data[i,:] = [ds1[i], ds2[i], ds3[i], ...]
 		for i in range (dsc):
-			self.plt.scatter (xlog, ylog[:,i], label=datalabel[i], alpha=0.5, color=colorlist[i])
+			plt.scatter (xlog, ylog[:,i], label=datalabel[i], alpha=0.5, color=colorlist[i])
 
 			# Best Fit Curve
 			c = np.polyfit(xlog, ylog[:,i], 1)
 			p = np.poly1d (c)
 			l = np.linspace (xlog[0], xlog[-1], num=100)
-			self.plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
+			plt.plot (l, p(l), '--', alpha=0.75, label=datalabel[i], color=colorlist[i])
 			equations.append ('{0[0]:.3f}*x {0[1]:+.3f}'.format(c))
 
-		self.ax.set_xlabel(kwargs.get('xlabel', 'Log10 of Document Size'))
-		self.ax.set_ylabel(kwargs.get('ylabel', 'Log10 of Average Runtime (ms)'))
-		self.ax.set_title(kwargs.get('title', 'Comparison Logarithmic Plot'))
+		ax.set_xlabel(kwargs.get('xlabel', 'Log10 of Document Size'))
+		ax.set_ylabel(kwargs.get('ylabel', 'Log10 of Average Runtime (ms)'))
+		ax.set_title(kwargs.get('title', 'Comparison Logarithmic Plot'))
 
-		self.ax.legend(loc='upper left')
-		self.plt.grid()
-		self.plt.show()
+		ax.legend(loc='upper left')
+		plt.grid()
+		plt.show()
 
 		if zflag:
 			print ('Warning: Data contains zero-values, data partially truncated to removed zero-valued points\n')
@@ -200,7 +280,7 @@ class PrettyPlot(object):
 		for i, e in enumerate(equations):
 			print (f'\t {datalabel[i]} \t= {e}')
 
-		return (self, c)
+		return (fig, ax)
 
 	"""
 	3D plotter for mapper-reducer processes
@@ -211,8 +291,9 @@ class PrettyPlot(object):
 	xstep and ystep is simply xtick and ytick values.
 	"""
 	def LandscapePlot (self, x, y, z, xstep, ystep, **kwargs):
-		self.fig = kwargs.get( 'fig', self.plt.figure(figsize=kwargs.get('figsize',(12,12))) )
-		self.ax = kwargs.get( 'ax', self.fig.add_subplot(kwargs.get('subplot',111) , projection='3d') )
+		(fig, ax) = kwargs.get('figax', plt.subplots(figsize=kwargs.get('figsize',(12, 12)), projection='3d'))
+		# fig = kwargs.get( 'fig', plt.figure(figsize=kwargs.get('figsize',(12,12))) )
+		# ax = kwargs.get( 'ax', fig.add_subplot(kwargs.get('subplot',111) , projection='3d') )
 
 		X = np.reshape(x, (xstep.size,ystep.size))
 		Y = np.reshape(y, (xstep.size,ystep.size))
@@ -220,37 +301,37 @@ class PrettyPlot(object):
 
 		# Plot Surface
 		cmap = kwargs.get('cmap',cm.jet)
-		surf = self.ax.plot_surface(X, Y, Z,
+		surf = ax.plot_surface(X, Y, Z,
 							   rstride=1, cstride=1, cmap=cmap, zorder=1, alpha=kwargs.get('alpha',1),
 							   linewidth=1, antialiased=True, shade=True)
 		cticks = np.arange(np.floor(np.min(z)), np.ceil(np.max(z)), kwargs.get('ctickstep',10) )
-		cbar = self.fig.colorbar(surf, shrink=kwargs.get('colorbar_shrink',1), aspect=12, ticks=cticks, extend='both')
+		cbar = fig.colorbar(surf, shrink=kwargs.get('colorbar_shrink',1), aspect=12, ticks=cticks, extend='both')
 		cbar.set_label('Avg time (ms)\n', rotation=270, fontsize=12, labelpad=20)
 
-		self.ax.set_xlabel( kwargs.get('xlabel','Map Processes (X)'), fontsize=kwargs.get('xlabel_fontsize',10) )
-		self.ax.set_ylabel( kwargs.get('ylabel','Reduce Processes (Y)'), fontsize=kwargs.get('ylabel_fontsize',10) )
-		self.ax.set_zlabel( kwargs.get('zlabel','Avg Time (ms)'), fontsize=kwargs.get('zlabel_fontsize',10) )
-		self.ax.set_title( kwargs.get('title','Map Reduce'), fontsize=kwargs.get('title_fontsize',10))
-		self.ax.view_init( kwargs.get('view_v',60), 180+kwargs.get('view_h',60))
+		ax.set_xlabel( kwargs.get('xlabel','Map Processes (X)'), fontsize=kwargs.get('xlabel_fontsize',10) )
+		ax.set_ylabel( kwargs.get('ylabel','Reduce Processes (Y)'), fontsize=kwargs.get('ylabel_fontsize',10) )
+		ax.set_zlabel( kwargs.get('zlabel','Avg Time (ms)'), fontsize=kwargs.get('zlabel_fontsize',10) )
+		ax.set_title( kwargs.get('title','Map Reduce'), fontsize=kwargs.get('title_fontsize',10))
+		ax.view_init( kwargs.get('view_v',60), 180+kwargs.get('view_h',60))
 
-		self.plt.xticks(np.arange(1,xstep.size+1))
-		self.plt.yticks(np.arange(1,ystep.size+1))
+		plt.xticks(np.arange(1,xstep.size+1))
+		plt.yticks(np.arange(1,ystep.size+1))
 
 		# Find Minimum
 		xmin = x[np.argmin(z)]
 		ymin = y[np.argmin(z)]
 		zmin = z[np.argmin(z)]
 		minLabel = "Minimum: ({0:d}, {1:d}, {2:.2f})".format(xmin, ymin, zmin)
-		minPoint = self.ax.scatter(xmin,ymin,zmin, label=minLabel, color='green', s=50, zorder=10)
+		minPoint = ax.scatter(xmin,ymin,zmin, label=minLabel, color='green', s=50, zorder=10)
 
 		# Find Maximum
 		xmax = x[np.argmax(z)]
 		ymax = y[np.argmax(z)]
 		zmax = z[np.argmax(z)]
 		maxLabel = "Maximum: ({0:d}, {1:d}, {2:.2f})".format(xmax, ymax, zmax)
-		maxPoint = self.ax.scatter(xmax,ymax,zmax, label=maxLabel, color='fuchsia', s=50, zorder=10)
+		maxPoint = ax.scatter(xmax,ymax,zmax, label=maxLabel, color='fuchsia', s=50, zorder=10)
 
-		self.ax.legend(loc='lower left')
+		ax.legend(loc='lower left')
 
 		return (self)
 
@@ -262,7 +343,7 @@ class PrettyPlot(object):
 	PLOT_NUMBER is which plot to draw in; raveled like: [[1,2],[3,4]] -> [1,2,3,4]
 	Y-AXIS is the sample size; all the provided datapoints are used to draw one "violin"
 	X-AXIS is the "cohort"; len(X-AXIS) > 1 would draw multiple "violins" on a single plot
-	Made this way, due to how self.plt.violinplot() works...
+	Made this way, due to how plt.violinplot() works...
 
 	-  input : int[][][] : 3D array of data for the violin plot
 	-  input : int : Number of rows in plot | works with value > 1
@@ -275,8 +356,8 @@ class PrettyPlot(object):
 		fig_height = 4 * nrows
 		fig_width = 16
 
-		fig, ax = self.plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width,fig_height))
-		self.plt.rcParams.update({'font.size': 10})
+		fig, ax = plt.subplots(nrows=nrows, ncols=ncols, figsize=(fig_width,fig_height))
+		plt.rcParams.update({'font.size': 10})
 
 		# Color map code: 0, 1, 2, 3
 		color_list = ['tab:red', 'tab:green', 'tab:blue', 'tab:orange']
